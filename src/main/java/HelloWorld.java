27c2000a45ead39854211d0f3270e03b6ad94397
rmurgu <no-reply>
2020-03-18 15:38:44 +0200

    Add source folder with HelloWolrd class

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Maven!!!");
+        System.out.println(StringUtils.capitalize("capitalize"));
+    }
+
+}
