ba69a01cbb8ab0a05b6d08821c088955e0ffdbc6
rmurgu <no-reply>
2020-03-18 15:49:24 +0200

    Rename HelloWorld into HelloMaven

--- src/main/java/HelloMaven.java
+++ src/main/java/HelloMaven.java
@@ -1,6 +1,6 @@
 import org.apache.commons.lang3.StringUtils;
 
-public class HelloWorld {
+public class HelloMaven {
 
     public static void main(String[] args) {
         System.out.println("Hello Maven!!!");
