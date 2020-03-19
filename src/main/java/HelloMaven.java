3e524716a8a0fd24542e1e26b370a5156334aef8
rmurgu <no-reply>
2020-03-19 11:25:37 +0200

    Make the jar executable

--- src/main/java/HelloMaven.java
+++ src/main/java/HelloMaven.java
@@ -1,10 +1,7 @@
-import org.apache.commons.lang3.StringUtils;
-
 public class HelloMaven {
 
     public static void main(String[] args) {
         System.out.println("Hello Maven!!!");
-        System.out.println(StringUtils.capitalize("capitalize"));
     }
 
 }
