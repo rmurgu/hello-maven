4e88b1c0bfe9013a224e27acffac5dfbf79e58fb
rmurgu <no-reply>
2020-03-19 11:05:23 +0200

    Add pojo test class

---
+++ src/test/java/HelloMavenTest.java
@@ -0,0 +1,10 @@
+public class HelloMavenTest {
+
+    public void testHello() {
+        System.out.println("Hello...");
+    }
+
+    public void testMaven() {
+        System.out.println("Maven...");
+    }
+}
